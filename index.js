353692da10bc38fb6aeb1144164a0553f900a6c6
Udokailodigwe <ilodigweudoka@gmail.com>
2022-06-04 04:44:06 +0300

    Created files for endpoints routes, installed necessary dependencies.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 import express from 'express';
+import bodyParser from 'body-parser'; // for taking incoming request bodies
+import morgan from 'morgan';
+import employeesRoutes from './routes/employees.js'; // usersRoute coming from default export users file
+
+const app = express(); //initialize express application
+const PORT = 5000;
+
+/*initialize middleware*/
+app.use(morgan('common')); //log requests using morgan common format.
+app.use(bodyParser.json());
+
+/*encapsulate the route to get all employees within express(app.use)*/
+app.use('/employees', employeesRoutes);
+
+/*...CRUD OPERATION...*/
+
+/*get homepage endpoint*/
+app.get('/', (req, res) => {
+    res.send('Hello and welcome to my index page');
+});
+
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
+
